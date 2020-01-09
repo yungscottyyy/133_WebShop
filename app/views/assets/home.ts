cb3d3792bcc410ce19672b2c0aecdcbef07d4b35
shouiii <joel.imfeld@gmail.com>
2020-01-09 11:26:12 +0100

    lul

--- app/views/assets/home.ts
+++ app/views/assets/home.ts
@@ -1,5 +1,14 @@
 import { Hero } from "../../types";
 
+var htmlElements = "";
+
+var numberOfProducts = 8;
+for (var i = 0; i < 8; i++) {
+   htmlElements += `<div class="col-sm-6 col-3"><img src="app/views/assets/images/product-${i}.jpg" /></div>`;
+}
+var container = document.getElementById("container");
+container.innerHTML = htmlElements;
+
 const tbody = document.querySelector("table tbody");
 fetch("/api/heroes")
     .then(r => r.json())
